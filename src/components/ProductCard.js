2eb353fcf8216d37ba39da67539b80e1177bdf77
tural2001 <you@example.com>
2023-03-12 00:29:31 +0400

    Shop

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -18,7 +18,16 @@ const ProductCard = (props) => {
           location.pathname === '/product' ? `gr-${grid}` : 'col-3'
         }`}
       >
-        <Link to="/product/:id" className="product-card position-relative ">
+        <Link
+          to={`${
+            location.pathname === '/product'
+              ? '/product/:id'
+              : location.pathname === '/product/:id'
+              ? '/product/:id'
+              : ':id'
+          }`}
+          className="product-card position-relative "
+        >
           <div className="wishlist-icon position-absolute">
             <button className="border-0 bg-transparent">
               <img src={wish} alt="wishlist" />
